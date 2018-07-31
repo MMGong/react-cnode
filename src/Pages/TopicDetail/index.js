02ca3f74688b12a3c8ae00c8599c9a7266738866
gonghao <gonghao114@sina.com>
2018-07-31 14:17:49 +0800

    m

--- src/Pages/TopicDetail/index.js
+++ src/Pages/TopicDetail/index.js
@@ -2,10 +2,10 @@
  * @Author: gonghao
  * @Date: 2018-07-30 23:04:10
  * @Last Modified by: gonghao
- * @Last Modified time: 2018-07-31 14:16:55
+ * @Last Modified time: 2018-07-31 14:17:35
  * @Desc: 文章详情
  */
-/* eslint-diable react/no-danger */
+/* eslint-diable react/no-danger  */
 import React, { Component } from 'react';
 import { observer, inject, PropTypes } from 'mobx-react';
 import dateFormat from 'date-fns/format';
