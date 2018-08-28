ca164a3d8e8508361c750932cacbdf58e13e5e7c
gonghao <1506301420@qq.com>
2018-08-28 10:32:33 +0800

    修改

--- src/Pages/TopicDetail/reply.js
+++ src/Pages/TopicDetail/reply.js
@@ -4,13 +4,24 @@ import PropTypes from 'prop-types';
 import distanceInWordsToNow from 'date-fns/distance_in_words_to_now';
 import zhLocale from 'date-fns/locale/zh_cn';
 import { Link } from 'react-router-dom';
+import { observer, inject } from 'mobx-react';
 
-
+@inject(stores => ({
+  upReply: stores.detailStore.upReply,
+  accessToken: stores.loginStore.accessToken,
+}))
+@observer
 export default class Reply extends Component {
   static propTypes = {
     list: PropTypes.arrayOf(PropTypes.object).isRequired,
+    upReply: PropTypes.func.isRequired,
+    accessToken: PropTypes.string.isRequired,
   }
   state = {}
+  up = (replyId, index) => {
+    if (!this.props.accessToken) return;
+    this.props.upReply(replyId, index, this.props.accessToken);
+  }
   render() {
     const list = this.props.list;
     return (
@@ -37,7 +48,10 @@ export default class Reply extends Component {
                       <span className="reply__item__user--item middle-dot">{index + 1}楼</span>
                       <span className="reply__item__user--item middle-dot">{distanceInWordsToNow(new Date(item.create_at), { locale: zhLocale })}前</span>
                     </span>
-                    <span className="float-right reply__item__icon">
+                    <span
+                      className={`float-right reply__item__icon ${item.is_uped && 'active'}`}
+                      onClick={() => this.up(item.id, index)}
+                    >
                       <i className="iconfont icon-zan reply__item__user--zan" />
                       <span>{item.ups.length}</span>
                     </span>
--- src/Store/detailStore.js
+++ src/Store/detailStore.js
@@ -2,11 +2,12 @@
  * @Author: gonghao
  * @Date: 2018-07-30 23:16:16
  * @Last Modified by: gonghao
- * @Last Modified time: 2018-07-30 23:51:54
+ * @Last Modified time: 2018-08-28 10:31:29
  * @Desc: 文章详情
  */
 
 import { observable, action, runInAction } from 'mobx';
+import loginStore from './loginStore';
 import apis from '../Api';
 
 class DetailStore {
@@ -14,7 +15,9 @@ class DetailStore {
 
   @action.bound
   getTopicDetail(id) { // 获取文章详情
-    return apis('getTopicDetail', {}, {
+    return apis('getTopicDetail', {
+      accesstoken: loginStore.accessToken,
+    }, {
       pathExtra: `/${id}`,
     }).then((data) => {
       if (data.success) {
@@ -28,6 +31,31 @@ class DetailStore {
   @action.bound
   clearTopicDetail() {
     this.topicDetail = {};
+  }
+
+  @action.bound
+  async upReply(replyId, index, accessToken) {
+    try {
+      const data = await apis('upsReply', {
+        accesstoken: accessToken,
+      }, {
+        pathExtra: `/${replyId}/ups`
+      });
+      if (data && data.success) {
+        const replies = this.topicDetail.replies;
+        runInAction(() => {
+          replies[index].is_uped = data.action === 'up';
+          if (data.action === 'up') {
+            replies[index].ups.push(loginStore.userInfo.id);
+          } else {
+            replies[index].ups.pop();
+          }
+          this.topicDetail.replies = replies;
+        });
+      }
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
 
